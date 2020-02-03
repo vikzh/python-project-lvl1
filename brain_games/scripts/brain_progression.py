b9a87bf3bbc2b8daec309ccea48d2fb9aa50677d
vikzh <vikt.anmail@gmail.com>
2020-02-03 17:46:22 +0300

    list filling refactored

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -13,21 +13,12 @@ def make_question_and_answer():
     progression_length = 10
     step = randrange(1, 10)
     start = randrange(100)
-    progression_elements = generate_progression(start=start,
-                                                step=step,
-                                                length=progression_length)
+    progression_elements = list(range(1, progression_length * step, step))
     random_progression_index = randrange(len(progression_elements))
     right_answer = progression_elements[random_progression_index]
     progression_elements[random_progression_index] = '...'
     question = ' '.join(map(str, progression_elements))
     return question, str(right_answer)
-
-
-def generate_progression(start, step, length):
-    list_ = [start]
-    for i in range(length - 1):
-        list_.append(list_[-1] + step)
-    return list_
 
 
 if __name__ == '__main__':
